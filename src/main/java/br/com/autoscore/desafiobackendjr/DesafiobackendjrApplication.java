bd63c5880068b5d9bd2a1e653ae932b9f675ee41
ruan rodrigues <rupriwell123@outlook.com>
2024-12-19 16:53:53 -0300

    Projeto criado os pacotes Entity,Dto,Service,Controller

---
+++ src/main/java/br/com/autoscore/desafiobackendjr/DesafiobackendjrApplication.java
@@ -0,0 +1,13 @@
+package br.com.autoscore.desafiobackendjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobackendjrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobackendjrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/autoscore/desafiobackendjr/DesafiobackendjrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autoscore.desafiobackendjr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobackendjrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
