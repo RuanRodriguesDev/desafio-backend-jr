a577d05c44ce147baf99cf16229d8d6842ac17dc
ruan rodrigues <rupriwell123@outlook.com>
2024-12-20 13:06:35 -0300

    Implementando [200~ @GetMapping(/error)

--- src/main/java/br/com/autoscore/desafiobackendjr/controller/ApiScoreController.java
+++ src/main/java/br/com/autoscore/desafiobackendjr/controller/ApiScoreController.java
@@ -4,6 +4,7 @@ package br.com.autoscore.desafiobackendjr.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +14,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.autoscore.desafiobackendjr.dto.ScoreDto;
 import br.com.autoscore.desafiobackendjr.entity.Score;
+import br.com.autoscore.desafiobackendjr.exception.GlobalExceptionHandler;
 import br.com.autoscore.desafiobackendjr.service.ApiServiceStore;
 import br.com.autoscore.desafiobackendjr.service.ScoreService;
+import jakarta.validation.Valid;
 
+@Valid
 @RestController
 @RequestMapping("/veiculos")
 public class ApiScoreController {
+   
+    private GlobalExceptionHandler globalExceptionHandler;
 
     private ApiServiceStore apiServiceStore;
     private ScoreService scoreService;
@@ -29,6 +36,11 @@ public class ApiScoreController {
         this.scoreService = scoreService;
     }
 
+    @GetMapping("/error")
+    public String triggerError() {
+        throw new RuntimeException("Erro de teste");
+    }
+
     @PostMapping
     public ResponseEntity<Score> salvarVeiculo(@RequestBody Score score){
         Score salvarScore = apiServiceStore.salvarVeiculo(score);
@@ -36,18 +48,18 @@ public class ApiScoreController {
     }
 
     @GetMapping
-     List<Score> listaVeiculo(){
-        List<Score> listaveiculo =  scoreService.getAllScores();
+     List<ScoreDto> listaVeiculo(){
+        List<ScoreDto> listaveiculo =  scoreService.getAllScores();
         return (listaveiculo);
     }
     @PutMapping("/{id}")
-    List<Score> updateVeiculo(@RequestBody Score score,@PathVariable("id")Long id){
+    List<ScoreDto> updateVeiculo(@RequestBody Score score,@PathVariable("id")Long id){
          return  scoreService.updadeScore(score);
      }
   
 
     @DeleteMapping("/{id}")
-    List<Score> deleteVeiculo(@PathVariable("id")Long id){
+    List<ScoreDto> deleteVeiculo(@PathVariable("id")Long id){
         return  scoreService.deleteScore(id);
     }
     
--- src/main/java/br/com/autoscore/desafiobackendjr/service/ScoreService.java
+++ src/main/java/br/com/autoscore/desafiobackendjr/service/ScoreService.java
@@ -1,9 +1,11 @@
 package br.com.autoscore.desafiobackendjr.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import br.com.autoscore.desafiobackendjr.dto.ScoreDto;
 import br.com.autoscore.desafiobackendjr.entity.Score;
 import br.com.autoscore.desafiobackendjr.repository.ScoreRepository;
 
@@ -16,16 +18,21 @@ public class ScoreService {
         this.scoreRepository = scoreRepository;
     }
 // Método para listar todas as pontuações
- public List<Score> getAllScores() {
-    return scoreRepository.findAll();
+ public List<ScoreDto> getAllScores() {
+    return scoreRepository.findAll().stream()
+    .map(score -> new ScoreDto(
+        null, score.getCpf(),
+        score.getProprietario(),
+        score.getPlaca())).collect(Collectors.toList());
+     
 }
-public List<Score> updadeScore(Score score){
+public List<ScoreDto> updadeScore(Score score){
     scoreRepository.save(score);
     return getAllScores();
 }
 
 
-public List<Score> deleteScore(Long id) {
+public List<ScoreDto> deleteScore(Long id) {
     scoreRepository.deleteById(id);
     return getAllScores();
 }
